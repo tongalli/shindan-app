97edf3dde26223a4750c5ee173b94575a344ffc4
tongalli <awano@awanokikaku.com>
2026-06-12 12:37:27 +0900

    Change export syntax from default to module.exports

--- api/save-result.js
+++ api/save-result.js
@@ -1,7 +1,7 @@
 // Vercel Serverless Function
 // 環境変数: NOTION_TOKEN, NOTION_DATABASE_ID
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
