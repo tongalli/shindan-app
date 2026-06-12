13121079dca0d82209a0c608e922fbf6d5f4f4d4
tongalli <awano@awanokikaku.com>
2026-06-12 14:56:32 +0900

    Update save-result.js

--- api/save-result.js
+++ api/save-result.js
@@ -8,9 +8,7 @@ module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   if (req.method === 'OPTIONS') return res.status(200).end();
 
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
+ 
 
   const { email, typeName, clusterV, clusterS, clusterD, clusterC, penetration } = req.body;
 
